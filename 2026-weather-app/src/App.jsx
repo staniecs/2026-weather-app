309b07c081b2a049429ab611f6c2c60285f77cd3
Sarah <145446078+staniecs@users.noreply.github.com>
2026-06-29 16:09:24 +0200

    added link to GitHub repo

--- 2026-weather-app/src/App.jsx
+++ 2026-weather-app/src/App.jsx
@@ -6,12 +6,12 @@ export default function App() {
    return (
 
      
-     <div>
+     <div className="App">
        <h1>Weather App</h1>
-        <p>
-           Sarah Staniec
-        </p>
+      
+        <footer>This project was coded by Sarah Staniec and is <a href="https://github.com/staniecs/2026-weather-app" target="_blank">open-sourced on GitHub</a>.</footer>
       </div>
+
     
   )
 }
