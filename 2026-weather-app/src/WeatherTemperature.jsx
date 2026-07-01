f9c2b87b987035e3a01a908dd2b84ede3a6c03dc
Sarah <145446078+staniecs@users.noreply.github.com>
2026-07-01 13:13:20 +0200

    Update WeatherTemperature.jsx
    added fahrenheit conversion

--- 2026-weather-app/src/WeatherTemperature.jsx
+++ 2026-weather-app/src/WeatherTemperature.jsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
     const [unit, setUnit] = useState(`celsius`);
-    function convertToF(event) {
+    function showF(event) {
         event.preventDefault();
         setUnit("fahrenheit")
+    }
+    function showC(event) {
+        event.preventDefault();
+        setUnit("celsius")
+    }
+
+    function fahrenheit() {
+        return (Math.round((props.celsius * 9/5) +32))
     }
     if (unit == "celsius") {
     return (
         <div className="WeatherTemperature">
               <span className="temperature">{props.celsius}</span>
-                        <span className="unit">°C | <a href="/" onClick={convertToF}>°F</a></span>
+                        <span className="unit">°C | <a href="/" onClick={showF}>°F</a></span>
         </div>
-    );}  else {return (<div className="WeatherTemperature">
-              <span className="temperature">{props.celsius}</span>
-                        <span className="unit"><a href="/" onClick={convertToF}>°C</a> | °F</span>
+    );}  else {
+     
+        return (<div className="WeatherTemperature">
+              <span className="temperature">{fahrenheit()}</span>
+                        <span className="unit"><a href="/" onClick={showC}>°C</a> | °F</span>
         </div>)}
 }
